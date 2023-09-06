da7ecb2b9cc97644bc7645dc58f6124d0d6f4779
root <root@gmail.com>
2023-09-06 21:22:23 +0545

    Almost finished

--- chapter13/LoginSys/LoginSystem.java
+++ chapter13/LoginSys/LoginSystem.java
@@ -5,7 +5,8 @@ import java.sql.*;
 
 class PanelElements extends JPanel{
 	private final JLabel l1, l2, l3, l4;
-	private final JTextField t1, t2;
+	private final JTextField t1;
+	private final JPasswordField t2;
 	private final JButton btn1, btn2;
 	private final JPanel p;
 	public PanelElements(){
@@ -14,7 +15,7 @@ class PanelElements extends JPanel{
 		l3 = new JLabel();
 		l4 = new JLabel();
 		t1 = new JTextField(14);
-		t2 = new JTextField(14);
+		t2 = new JPasswordField(14);
 		btn1 = new JButton("Register");
 		btn2 = new JButton("Login");
 		p = new JPanel(new GridLayout(7, 1));
@@ -63,20 +64,56 @@ class PanelElements extends JPanel{
 	}
 	private class ValidRegister extends MouseAdapter{
 		public void mousePressed(MouseEvent e){
-			String username = t1.getText();
+			String username = t1.getText().toLowerCase();
                         String password = t2.getText();
                         if(username.isEmpty() || password.isEmpty()){
                                 l4.setText("Invalid Credentials!");
-                        }
+                        }else{
+				try{
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "");
+					Statement stmt = conn.createStatement();
+					String sqlStmt = "INSERT INTO user VALUE("+"'"+username+"'"+","+"'"+password+"'"+");";
+					stmt.executeUpdate(sqlStmt);
+					l4.setText("User registered successfully!");
+					t1.setText("");
+					t2.setText("");
+					stmt.close();
+					conn.close();
+				}catch(Exception err){
+					System.out.println(err);
+				}
+			}
 		}
 	}
 	private class ValidLogin extends MouseAdapter{
 		public void mousePressed(MouseEvent e){
-			String username = t1.getText();
+			String username = t1.getText().toLowerCase();
                         String password = t2.getText();
                         if(username.isEmpty() || password.isEmpty()){
                                 l4.setText("Invalid Credentials!");
-                        }
+                        }else{
+				try{
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "");
+					Statement stmt = conn.createStatement();
+					String sqlStmt = "SELECT password FROM user WHERE username = "+"'"+username+"'"+";";
+					ResultSet rs = stmt.executeQuery(sqlStmt);
+                    String user_pass = "";
+                    while(rs.next()){
+					  user_pass = rs.getString(1);
+                    }
+                    if(password.equals(user_pass)){
+                      l4.setText("Login Successful!");
+                    }else{
+                      l4.setText("Login failed!");
+                    }
+					stmt.close();
+					conn.close();
+				}catch(Exception err){
+					System.out.println(err);
+				}
+			}
 		}
 	}
 }
